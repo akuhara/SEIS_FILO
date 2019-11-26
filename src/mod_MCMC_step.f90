e822cc53853bf4e49bd540fd7d6a10f78f51f05a
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2019-11-26 15:29:24 +0900

    add MCMC step

---
+++ src/mod_MCMC_step.f90
@@ -0,0 +1,33 @@
+module mod_MCMC_step
+  use mod_random
+  use mod_trans_d_model
+  implicit none 
+  
+contains
+
+  !---------------------------------------------------------------------
+  
+  subroutine propose_model(tm_current, tm_proposed)
+    type(trans_d_model), intent(inout) :: tm_current
+    type(trans_d_model), intent(out) :: tm_proposed
+    integer :: nparam, itype, iparam
+    logical :: is_ok
+    
+    nparam = tm_current%get_n_x()
+    itype = int(rand_u() * nparam + 2)
+    tm_proposed = tm_current
+    if (itype == 0) then
+       ! Birth
+       call tm_proposed%birth(is_ok)
+    else if (itype == 1) then
+       ! Death
+       
+    end if
+
+
+    
+  end subroutine propose_model
+
+  !---------------------------------------------------------------------
+  
+end module mod_MCMC_step
---
+++ src/test_MCMC_step.f90
@@ -0,0 +1,8 @@
+program main
+  use mod_random
+  use mod_trans_d_model
+  use mod_MCMC_step
+  implicit none 
+
+  stop
+end program main
