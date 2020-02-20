ba1485c444a9cdcc7662ff0522587a0a4f99876a
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2020-02-20 13:46:14 +0900

    plot utilities are modified for multiple dispersion inputs

--- util/plot_rayleigh_inv.py
+++ util/plot_rayleigh_inv.py
@@ -1,18 +1,16 @@
 import sys
 from sub import filoplot 
-
-
-
         
 #-----------------------------------------------------------------------
 
 if __name__ == "__main__":
     # Get argument
     args = sys.argv
-    if len(args) != 2:
-        print("USAGE: plot.py [parameter file]")
+    if len(args) != 3:
+        print("USAGE: plot.py [parameter file] [dispersion curve ID]")
         sys.exit()
         
     param_file = args[1]
+    curve_id = int(args[2])
     rslt = filoplot.InvResult(param_file)
-    rslt.draw_surface_wave_set()
+    rslt.draw_surface_wave_set(curve_id)
