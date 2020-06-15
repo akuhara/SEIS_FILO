30be11d326c9c126be4b7a6659b44809118af6c5
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2020-06-15 14:46:51 +0900

    n=1024

--- util/sub/filoplot.py
+++ util/sub/filoplot.py
@@ -183,8 +183,9 @@ class InvResult:
         t_min = float(param["t_start"])
 
         del_t = float(param["delta"])
-        t_max = 2 * (float(param["t_end"]) - float(param["t_start"])) \
-                + float(param["t_start"]) - del_t    
+        #t_max = 2 * (float(param["t_end"]) - float(param["t_start"])) \
+        #        + float(param["t_start"]) - del_t    
+        t_max = float(param["t_start"]) + 1024 * del_t
         print(t_min, t_max)
         a, t = np.mgrid[slice(amp_min, \
                               amp_max + del_amp, \
